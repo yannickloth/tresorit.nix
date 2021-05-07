215c1e229b1c8a328089575e0bb084bdd69d764d
Yannick Loth <yannickloth@users.noreply.github.com>
2021-05-07 13:05:43 +0200

    update tresorit to version 3.5.871.1720, add libfuse to rpath

--- default.nix
+++ default.nix
@@ -4,21 +4,25 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
 
-  version = "3.5.852.1550";
+
+#  version = "3.5.852.1550";
+  version = "3.5.871.1720";
   name = "tresorit-${version}";
 
   src = fetchurl {
+#    url = https://installerstorage.blob.core.windows.net/public/install/tresorit_installer.run;
     url = https://installerstorage.blob.core.windows.net/public/install/tresorit_installer.run;
-    sha256 = "18507prr228q0csfqri4k9yiyzmrinzx5rxc67acvf710qzsqn6r";
+#    sha256 = "18507prr228q0csfqri4k9yiyzmrinzx5rxc67acvf710qzsqn6r";
+    sha256 = "be118019401ca43bfd28e4141bda533b87af92b46c324ac9cec643894717ef97";
   };
 
+  runtimeDependencies = [ fuse ];
+  dontPatchELF = true;
   nativeBuildInputs = [ autoPatchelfHook ];
   buildInputs = [ xorg.libXext
                   xorg.libxcb
                   xorg.libX11
-                  fuse
                   libGL ];
-
   dontBuild = true;
   dontConfigure = true;
   dontMake = true;
@@ -33,7 +37,7 @@ stdenv.mkDerivation rec {
     rm $out/bin/uninstall.sh
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Tresorit is the ultra-secure place in the cloud to store, sync and share files easily from anywhere, anytime.";
     homepage = https://tresorit.com;
     license = licenses.unfree;
