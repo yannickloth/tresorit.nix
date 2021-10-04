574cea6ed58bb050c5c199103bd0f855fc4f3640
Balazs Lengyel <balazs.lengyel@carboncloud.com>
2021-10-04 21:58:36 +0200

    update to latest version

--- default.nix
+++ default.nix
@@ -4,12 +4,12 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
 
-  version = "3.5.871.1720";
+  version = "3.5.938.2150";
   name = "tresorit-${version}";
 
   src = fetchurl {
     url = https://installerstorage.blob.core.windows.net/public/install/tresorit_installer.run;
-    sha256 = "be118019401ca43bfd28e4141bda533b87af92b46c324ac9cec643894717ef97";
+    sha256 = "0m83zb59v1fkyrc9lkyggiifcrh5mfxg579bk8512szypn7m83mp";
   };
 
   runtimeDependencies = [ fuse ];
